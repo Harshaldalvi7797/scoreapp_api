50e2d2adc85c3d16113064b7547ba3ec1812a990
Harshaldalvi7797 <dalviharshal7797@gmail.com>
2022-06-24 20:51:36 +0530

    connection establish on port done

---
+++ app.js
@@ -0,0 +1,13 @@
+let express = require("express")
+let app = express()
+
+
+
+
+
+
+const PORT= 4000
+//port connection
+app.listen(PORT, () => {
+    console.log(`connected to port ${PORT}`)
+  });
